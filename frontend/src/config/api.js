af6f3691984f6224a5e016e09ff58c65c0638de5
Vikash-Potnuru <vikashpotnuru94@gmail.com>
2026-09-07 18:46:01 +0530

    added vite prefix

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,4 @@
-export const API_BASE_URL = import.meta.env.API_BASE_URL
-export const SOCKET_URL = import.meta.env.SOCKET_URL
+export const API_BASE_URL =
+	import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'
+export const SOCKET_URL =
+	import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000'
